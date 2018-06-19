303454007f4150d7230ade325c37c0e826473683
Adam Chlebek <adamchlebek@live.com>
2018-06-19 09:23:27 -0400

    New Icon and Signing

--- TypingClub Parse/My Project/Resources.Designer.vb
+++ TypingClub Parse/My Project/Resources.Designer.vb
@@ -63,16 +63,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property checkmark() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("checkmark", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property Upload() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Upload", resourceCulture)
